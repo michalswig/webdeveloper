7b310298a9f3842f085269122297bf430dcac344
michalswig <mike@data.pl>
2024-08-02 14:30:46 +0200

    add: config files

---
+++ src/main/java/com/mike/webdeveloper/WebdeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.mike.webdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdeveloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mike/webdeveloper/WebdeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.webdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
